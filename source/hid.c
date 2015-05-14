76997590c71f8599dc202cdc2f4a58bc7d443f28
Yuri Kunde Schlesner <yuriks@yuriks.net>
2015-05-14 04:25:25 -0300

    Fix key input function continuing on button releases

--- source/hid.c
+++ source/hid.c
@@ -4,7 +4,8 @@ u32 InputWait() {
     u32 pad_state_old = HID_STATE;
     while (true) {
         u32 pad_state = HID_STATE;
-        if (pad_state ^ pad_state_old)
+        if ((pad_state ^ pad_state_old) & pad_state)
             return ~pad_state;
+        pad_state_old = pad_state;
     }
 }
