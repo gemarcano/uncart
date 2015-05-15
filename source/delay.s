7e785433d69c9a3be3a4265d460b3df1df3484fb
Yuri Kunde Schlesner <yuriks@yuriks.net>
2015-05-15 19:26:14 -0300

    Do uncached reads inside ioDelay to improve delay reliability

--- source/delay.s
+++ source/delay.s
@@ -8,6 +8,10 @@
 
 @ioDelay ( u32 us )
 ioDelay:
+	ldr r1, =0x18000000  @ VRAM
+1:
+	@ Loop doing uncached reads from VRAM to make loop timing more reliable
+	ldr r2, [r1]
 	subs r0, #1
-	bgt ioDelay
+	bgt 1b
 	bx lr
